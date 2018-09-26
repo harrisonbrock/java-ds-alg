c1653bdc6a1978d119280301067f1700a2cef388
Harrison Brock <harrison@harrisonbrock.com>
2018-09-26 13:24:15 -0400

    ADDED: Bubble Sort

--- src/com/harrisonbrock/Main.java
+++ src/com/harrisonbrock/Main.java
@@ -4,6 +4,39 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello");
+        int[] numbers = {20,-22,35,-15,7,65,1};
+
+//        for (int number : numbers) {
+//            System.out.println(number);
+//        }
+//
+//        bubbleSort(numbers);
+//        System.out.println("Sorted");
+//        for (int number : numbers) {
+//            System.out.println(number);
+//        }
+
+    }
+
+    public static void bubbleSort(int[] array) {
+
+        int lastIndex = array.length - 1;
+        for (; lastIndex > 0; lastIndex--) {
+
+            for (int index = 0; index < lastIndex; index++) {
+                if (array[index] > array[index + 1]) {
+                    swap(array, index, index+1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] array, int x, int y) {
+
+        if (x == y) return;
+
+        int temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
     }
 }
